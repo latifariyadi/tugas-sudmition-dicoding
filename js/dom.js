90ead030fe5768fff594ee9efe041454351cc69a
jemblonganvalley <jemblonganvalley@gmail.com>
2021-07-26 12:48:07 +0800

    update

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,9 @@
 const UNCOMPLETED_LIST_TODO_ID = "incompleteBookshelfList";
 
+var toSelesai = false;
+var data = [];
+var inData = [];
+
 function makeTodo(id, judul, penulis, tahun) {
   const idBuku = document.createElement("h2");
   idBuku.innerText = id;
@@ -35,10 +39,74 @@ function addTodo() {
   console.log("todo" + penulis);
   console.log("todo" + tahun);
 
-  const todo = makeTodo(id, judul, penulis, tahun);
+  // const todo = makeTodo(id, judul, penulis, tahun);
+  const inComplete = document.getElementById("incompleteBookshelfList");
+  const complete = document.getElementById("completeBookshelfList");
+
+  console.log(data);
+
+  if (toSelesai == true) {
+    data.push({
+      id: id,
+      judul: judul,
+      penulis: penulis,
+      tahun: tahun,
+    });
+    data.map((e, i) => {
+      complete.innerHTML += `
+    <article class="book_item" id="${e.id}">
+      <p>id : ${e.id}</p>
+      <h1>${e.judul}</h1>
+      <p>penulis : ${e.penulis}</p>
+      <p>tahun : ${e.tahun}</p>
+      <div class="action">
+        <button class="green" onclick="completeHandle(event)" value="${e.id}">selesai</button>
+        <button class="red" onclick="handleDelete(event)" value="${e.id}">hapus</button>
+      </div>
+    </article>
+  `;
+    });
+  } else {
+    inData.push({
+      id: id,
+      judul: judul,
+      penulis: penulis,
+      tahun: tahun,
+    });
+    inComplete.innerHTML += `
+    <article class="book_item" id="${id}">
+      <p>id : ${id}</p>
+      <h1>${judul}</h1>
+      <p>penulis : ${penulis}</p>
+      <p>tahun : ${tahun}</p>
+      <div class="action">
+        <button class="green" onclick="completeHandle(event)" value="${id}">selesai</button>
+        <button class="red" onclick="handleDelete(event)" value="${id}">hapus</button>
+      </div>
+    </article>
+  `;
+  }
+
   unreadBook.append(todo);
   console.log(todo);
 }
+
+window.handleChange = (event) => {
+  toSelesai = !toSelesai;
+  console.log(toSelesai);
+};
+
+window.completeHandle = (event) => {
+  const article_id = document.getElementById(event.target.value);
+  const book_list = document.getElementById("completeBookshelfList");
+  // console.log(`${article_id}`);
+  book_list.append(article_id);
+};
+
+window.handleDelete = (event) => {
+  const article_id = document.getElementById(event.target.value);
+  article_id.remove();
+};
 
 function createButton(buttonClass, eventListener) {
   const button = document.createElement("button");
